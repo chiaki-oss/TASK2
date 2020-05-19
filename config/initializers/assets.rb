0a34e53b6e2fcaabf0eb688f6f3700e972b51231
渡辺　千晶 <chiaki@watanabechiakinoMacBook-Air.local>
2020-05-19 12:45:26 +0900

    completed

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,3 +15,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 Rails.application.config.assets.precompile += %w(home.css)
 Rails.application.config.assets.precompile += %w(edit.css)
+Rails.application.config.assets.precompile += %w(show.css)
